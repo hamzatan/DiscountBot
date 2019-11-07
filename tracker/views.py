e8eae6c2a7ce30f268db775ef54dabf118d0f4f4
hamzatan.ht@outlook.com <hamzatan.ht@outlook.com>
2019-11-06 19:37:58 -0500

    Updated search function to use Django builtin forms,  using code block, views.py to check GET and POST requests and forms.py to set the items in the form

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import NameForm
 
+# Homepage that is fetched initially
 def homepage(request):
-    return render(request,'tracker/index.html')
+    form = NameForm() # Initializes an empty form on inital load
+    return render(request,'tracker/index.html', {'form': form})
+
+# Takes input from the search bar and processes the data
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            pass  
+    else:
+        form = NameForm() 
+
+    return render(request, 'tracker/index.html', {'form': form})
